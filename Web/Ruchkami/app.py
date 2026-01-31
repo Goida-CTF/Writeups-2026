01687d86740eba0ff6434c803d782d78bc752520
depost <alex.vedeniapin@gmail.com>
2026-01-31 16:01:16 +0300

    Adding headless

--- Web/Ruchkami/app.py
+++ Web/Ruchkami/app.py
@@ -32,7 +32,7 @@ def get_url():
     url = data['url']
     
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("start-maximized")
